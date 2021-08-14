a381d163606f536835803a63813b16ef7b9ecd54
Erik Oja <erik_oja@yahoo.com>
2021-08-14 18:19:32 -0500

    add register view

--- game/views.py
+++ game/views.py
@@ -1,4 +1,41 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from .models import User
 
 def index(request):
     return render(request, "game/index.html")
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        confirmation = request.POST["confirmPassword"]
+
+        # Ensure password matches confirmation
+        if password != confirmation:
+            return render(request, "game/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "game/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))   
+    else:
+        return render(request, "game/register.html")
+
+
+def login(request):
+    return render(request, "game/login.html")
