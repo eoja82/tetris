f6472b43e37055095606e1bfa735183adf4022af
Erik Oja <erik_oja@yahoo.com>
2021-08-15 16:53:47 -0500

    add model Scores

--- game/models.py
+++ game/models.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 
 class User(AbstractUser):
     pass
+
+class Scores(models.Model):
+    user = models.ForeignKey(User, on_delete=CASCADE)
+    score = models.IntegerField()
