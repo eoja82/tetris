883961493672fe7a3dd47b01ccfe5c85c080f42e
Erik Oja <erik_oja@yahoo.com>
2021-08-17 12:47:02 -0500

    add path score

--- game/urls.py
+++ game/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
   path("", views.index, name="index"),
-  path("register", views.register, name="register"),
   path("login/", views.login_user, name="login"),
-  path("logout", views.logout_user, name="logout")
+  path("logout", views.logout_user, name="logout"),
+  path("register", views.register, name="register"),
+  path("score", views.score, name="score")
 ]
